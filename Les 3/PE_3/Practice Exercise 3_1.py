8a45d07dd6541acfbe41729b6a567126841d236a
SFenijn <stijn.fenijn@student.hu.nl>
2017-09-14 10:03:17 +0200

    Practice Exercise Les 1

---
+++ Les 3/PE_3/Practice Exercise 3_1.py
@@ -0,0 +1,6 @@
+# Als de persoon een score > 15 heeft, dat is de persoon geslaagt.
+score = float(input('Wat is uw score?'))
+if score > 15:
+    print('Gefeliciteerd! U bent geslaagd.')
+else:
+    print('Helaas u bent niet geslaagd.')
